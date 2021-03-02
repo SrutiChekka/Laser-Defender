6a9296b26a9774ad2bc69d9326374123882ad80a
SrutiChekka <SrutiChekka@users.noreply.github.com>
2021-03-02 22:12:19 +0530

    Add empty project

---
+++ Laser Defender/Assets/Scripts/ExtraLife.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraLife : MonoBehaviour
+{
+    [SerializeField] int extraHealth = 100;
+
+    private void OnTriggerEnter2D()
+    {
+        FindObjectOfType<Player>().LifeUp(extraHealth);
+        Destroy(gameObject);
+    }
+}
